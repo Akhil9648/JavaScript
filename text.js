baf81d80d730e219f7cc273eaa5eb0c608236ff6
Akhil Pandey <akhilpandey494@gmail.com>
2025-01-07 16:06:09 +0000

    Push

--- text.js
+++ text.js
@@ -1,1 +1,10 @@
+"use strict"; //Treat all js code as newer version
 console.log("Akhil Pandey")
+// Documentation-tc39,mdn(by mozzila)
+// Number-2 to power 53
+// Bigint bigdata used in stock markets EventCounts
+// String
+// Boolean true or false
+// null value and datatype
+// Undefined ==>value not defined
+// Symbol=>Unique
